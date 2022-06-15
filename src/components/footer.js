c1da33e1ba49687027b137c1fb54dc3f467ab6de
Decryptr <decryptr@Decryptrs-MacBook-Pro.local>
2022-06-14 23:17:30 -0400

    Final Push 2

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,19 +7,17 @@ const Footer = () => {
                         <h3>Quick Links</h3>
                             <a href="#home"> <i className="fa fa-arrow-right"></i>Home</a>
                             <a href="#mint"> <i className="fa fa-arrow-right"></i>Mint</a>
-                            <a href="#rarities"> <i className="fa fa-arrow-right"></i>Rarities</a>
-                            <a href="#chickencoop"> <i className="fa fa-arrow-right"></i>BinkieVerse</a>
+                            <a href="#Binkieverse"> <i className="fa fa-arrow-right"></i>BinkieVerse</a>
                             <a href="#roadmap"> <i className="fa fa-arrow-right"></i>Roadmap</a>
                             <a href="#faq"> <i className="fa fa-arrow-right"></i>FAQ</a>
                     </div>
 
                     <div className="box">
                         <h3>Extra Links</h3>
-                        <a href="https://medium.com/@cluckies/are-you-going-to-be-a-lucky-mother-clucker-c6bf7ab5adf0"> <i className="fa fa-arrow-right"></i> Medium Page</a>
                         <a href="https://opensea.io/"> <i className="fa fa-arrow-right"></i> OpenSea</a>
                         <a href="https://etherscan.io/"> <i className="fa fa-arrow-right"></i> Etherscan</a>
                         <a href="https://looksrare.org/"> <i className="fa fa-arrow-right"></i> Looks Rare</a>
-                        <a href="#home"> <i className="fa fa-arrow-right"></i> Binkies Lab</a>
+                        <a href="https://binkies.io/"> <i className="fa fa-arrow-right"></i> Binkies Lab</a>
                     </div>
                     
                     <div className="box">
