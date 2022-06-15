fdcf76ffdd5d2605a19c736e4f63b5c015a857a1
Decryptr <decryptr@Decryptrs-MacBook-Pro.local>
2022-06-15 13:17:18 -0400

    Final Push 3

--- src/components/binkieverse.js
+++ src/components/binkieverse.js
@@ -10,19 +10,19 @@ const Binkieverse = () => {
                 <div className="content">
                     <h3>About Binkie Babies</h3>
                     <p>
-                    You know how the genesis Binkies collection said they are hopping around and banging on the mainnet?
+                    You know how the genesis Binkies collection said they are hopping around and banging on the Ethereum blockchain?
                     Well... some Binkies weren't playing it very safe and now have <strong>Binkie Babies</strong>, a collection of 3,333
-                    babies trying to find their parents on the mainnet.
+                    babies trying to find their parents out in the wild west of web3.
                     </p>
                     <br></br>
                     <p>
-                    We reduced the amount of traits to really bring out the cuteness in our colleciton of Binkie Babies. but other than the cute
+                    We reduced the amount of traits to really bring out the cuteness in our colleciton of Binkie Babies. But other than the cute
                     faces we left all the other traits so you can find your Binkies offspring! (Or get another chance to pull a SUPER RARE)
                     </p>
                     <br></br>
                     <p>
                     We are working directly with the Binkies founders to make sure that this collection follows the high standards that Binkies 
-                    genesis collection has set. This generation of Binkies will be a huge advocate to how awesome BinkieVille and Binkie Academy is!
+                    Genesis collection has set. This generation of Binkies will be a huge advocate to how awesome BinkieVille and Binkie Academy is!
                     </p>
                     <br></br>
                     <p>
